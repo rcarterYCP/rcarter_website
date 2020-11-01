cda74a64975e4dc3e36719122314eec80e4c83c3
Tim McKenna - YCP <47125817+YCPTimMcKenna@users.noreply.github.com>
2020-11-01 07:18:12 -0800

    TIM EDITS/Added Off Canvas Menu + Merch Page
    What I did: 
    - Added in html and css structure and styling for index.html (currently hidden, but we'll show with media queries)
    - updates your merch page to have things align in a grid
    
    What you need to do:
    - Get all of your images in the img folder
    - Started getting the content into the other pages

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-// Put your custom Javascript here
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginRight = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginRight= "0";
+  }
